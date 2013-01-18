9010c6f86994e2111581a43cac9f800cadbdf228
raman mittal <rmittal@greenapplesolutions.com>
2013-01-18 21:19:23 +0530

    added BarTargetModel.cs

---
+++ trunk/DMP/DMP/Models/BarTargetModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DMP.Models {
+    public class BarTargetModel {
+        public string Month { get; set; }
+        public int T1 { get; set; }
+        public int T2 { get; set; }
+        public int Actual { get; set; }
+    }
+
+    public class BarTargetModels {
+        public IEnumerable<BarTargetModel> Models { get; set; }
+    }
+}
